74036207ad72ba6ce20a8c36c3564982a9e15e93
Veera Pirla <17177346+mrveera@users.noreply.github.com>
2023-12-10 12:17:11 +0530

    test go module

--- sqlcore/dml.go
+++ sqlcore/dml.go
@@ -3,23 +3,28 @@ package sqlcore
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/mrveera/sxt-sdks/helpers"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"sxt-sdks/helpers"
 )
 
 // Run all DML queries
-func DML(sqlText, originApp  string, biscuitArray []string, resources []string) (errMsg string, status bool){
+func DML(sqlText, originApp string, biscuitArray []string, resources []string, accesstoken string) (errMsg string, status bool) {
 	apiEndPoint, _ := helpers.ReadEndPointGeneral()
 	tokenEndPoint := apiEndPoint + "/sql/dml"
 
+	at := accesstoken
+
+	if accesstoken == "" {
+		at = os.Getenv("accessToken")
+	}
 
 	client := http.Client{}
 	postBody, _ := json.Marshal(map[string]interface{}{
-		"biscuits": biscuitArray,
+		"biscuits":  biscuitArray,
 		"resources": resources,
-		"sqlText":    sqlText,
+		"sqlText":   sqlText,
 	})
 
 	responseBody := bytes.NewBuffer(postBody)
@@ -29,11 +34,10 @@ func DML(sqlText, originApp  string, biscuitArray []string, resources []string) 
 		return err.Error(), false
 	}
 
-	req.Header.Add("Authorization", "Bearer "+os.Getenv("accessToken"))
+	req.Header.Add("Authorization", "Bearer "+at)
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("originApp", originApp)
-	
 
 	res, err := client.Do(req)
 	if err != nil {
